96d1fe8aa710cbdfd7541e56e053665d4970acad
PragatiGund <gundpragati@gmail.com>
2026-03-06 17:39:30 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+public class LeapYear {
+ public static void main(String[]args) {
+	int Leap = 2011;
+	if (Leap>2022) {
+		System.out.println("is a leap year" );
+	}
+	else {
+		System.out.println("is not leap year");
+	}
+ }
+}
